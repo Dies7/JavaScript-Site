b99d92cb9043446c7a50c597f0a04f8d0d96cc96
Dies7 <ed131193@students.davenport.k12.ia.us>
2017-10-11 13:13:38 -0500

    update

--- script.js
+++ script.js
@@ -121,6 +121,7 @@ var elem = document.querySelectorAll(".num");
 toggle between hiding and showing the dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("javabutton").innerHTML = "JavaScript is okay.";
 }
 
 // Close the dropdown menu if the user clicks outside of it
@@ -137,3 +138,39 @@ window.onclick = function(event) {
     }
   }
 }
+
+// Set up!
+var a_canvas = document.getElementById("a");
+var context = a_canvas.getContext("2d");
+
+// Draw the face
+context.fillStyle = "yellow";
+context.beginPath();
+context.arc(95, 85, 40, 0, 2*Math.PI);
+context.closePath();
+context.fill();
+context.lineWidth = 2;
+context.stroke();
+context.fillStyle = "black";
+
+// Draw the left eye
+context.beginPath();
+context.arc(75, 75, 5, 0, 2*Math.PI);
+context.closePath();
+context.fill();
+
+// Draw the right eye
+context.beginPath();
+context.arc(114, 75, 5, 0, 2*Math.PI);
+context.closePath();
+context.fill();
+
+// Draw the mouth
+context.beginPath();
+context.arc(95, 90, 26, Math.PI, 2*Math.PI, true);
+context.closePath();
+context.fill();
+
+// Write "Hello, World!"
+context.font = "30px Garamond";
+context.fillText("Hello, World!",15,175);
